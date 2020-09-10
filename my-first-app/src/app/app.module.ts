50c74a0c28b649211cb7084b4a0ff929a2153be7
Mateusz <wirus006@gmail.com>
2020-09-10 12:36:36 +0200

    new component: success-alert works

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './alerts/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './alerts/success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningAlertComponent
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
     imports: [
         BrowserModule
