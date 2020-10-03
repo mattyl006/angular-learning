c03df22362ba0dedb2db1d3ad754db9070fffb8b
Mateusz <wirus006@gmail.com>
2020-10-03 11:02:09 +0200

    string interpolation test

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './alerts/warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './alerts/success-alert/success-alert.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    ServerComponent,
+    ServersComponent
   ],
     imports: [
         BrowserModule
---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.scss']
+})
+export class ServersComponent implements OnInit {
+  constructor() { }
+
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
+  }
+}
