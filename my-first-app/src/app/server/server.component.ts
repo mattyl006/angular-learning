ddc2fb953ecc529d7cab02b9266e446a13e61e7e
Mateusz <wirus006@gmail.com>
2020-10-07 11:18:59 +0200

    test ngClass directive: add some styles if status is online

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.scss']
 })
 export class ServerComponent {
   serverId: number = 10;
